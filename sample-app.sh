0fc46ad40086ca8c17bdcab98d7819561c14b0d1
NetAcad DEVASC <changeme@example.com>
2026-05-20 02:42:36 +0000

    Docker y CI/CD

---
+++ sample-app.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+docker stop samplerunning 2>/dev/null
+docker rm samplerunning 2>/dev/null
+
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
+
+cp sample_app.py tempdir/.
+cp -r templates/* tempdir/templates/.
+cp -r static/* tempdir/static/.
+
+echo "FROM python:3.10-slim" > tempdir/Dockerfile
+echo "RUN pip install --no-cache-dir --progress-bar off flask" >> tempdir/Dockerfile
+echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "EXPOSE 6789" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
+
+cd tempdir
+docker build -t sampleapp .
+docker run -t -d -p 6789:6789 --name samplerunning sampleapp
+docker ps -a
